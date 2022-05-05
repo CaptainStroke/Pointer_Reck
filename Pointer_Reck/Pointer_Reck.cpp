2f66633eeebf9e2f84a809059c863d71b39b2be1
Glib Petrachkov <petrasha@gmail.com>
2022-05-05 09:14:38 +0300

    Добавьте файлы проекта.

---
+++ Pointer_Reck/Pointer_Reck.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void rect(int width, int height, int* sq, int* p)
+{
+	*sq = width * height;
+	*p = (width + height) * 2;
+	cout << *sq << endl;
+	cout << *p << endl;
+}
+
+int main()
+{
+	int w = 10;
+	int h = 5;
+	int s;
+	int p;
+	rect(w, h, &s, &p);
+}
